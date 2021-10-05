2413177701e02f66b0d5eede211b0217e583919e
Jonatan Lindh <jonatan.lindh1@gmail.com>
2021-10-05 21:31:08 +0200

    const pin numbers

--- robot/playground/test/test.ino
+++ robot/playground/test/test.ino
@@ -1,35 +1,38 @@
+const int enPin = 10;
+const int pin1 = 9;
+const int pin2 = 12;
+
 void setup()
 {
-  // pinMode(13, OUTPUT);
-  pinMode(12, OUTPUT);
-  pinMode(9, OUTPUT);
+  pinMode(pin1, OUTPUT);
+  pinMode(pin2, OUTPUT);
 }
 
 void loop()
 {
   go(true, 2);
   //delay(2000);
-  //go(false, 9);
+  //go(false, pin2);
   //delay(2000);
 }
 
-void go(bool cc, int speed_control){
+void go(bool cc, int speed_control)
+{
   int high, low;
-  if (cc) {
-    high = 9;
-    low = 12;
-  } else {
-    high = 12;
-    low = 9;
+
+  if (cc)
+  {
+    high = pin2;
+    low = pin1;
   }
-  if (speed_control <= 3) {
-    analogWrite(10, 250);
-    digitalWrite(low, LOW);
-    digitalWrite(high, HIGH);
+  else
+  {
+    high = pin1;
+    low = pin2;
   }
-  digitalWrite(high, LOW);
-  int speed = map(speed_control, 0, 9, 0, 255);
-  analogWrite(10, speed);
+
+  int speed = map(speed_control, 0, pin2, 0, 255);
+  analogWrite(enPin, speed);
 
   digitalWrite(low, LOW);
   digitalWrite(high, HIGH);
