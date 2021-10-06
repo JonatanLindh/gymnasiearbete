a6bd6951f0566af604cdc6f0a2826db0554abca0
Jonatan Lindh <jonatan.lindh1@gmail.com>
2021-10-06 09:49:08 +0200

    create Motor class

--- robot/playground/test/test.ino
+++ robot/playground/test/test.ino
@@ -1,39 +1,56 @@
-const int enPin = 10;
-const int pin1 = 9;
-const int pin2 = 12;
+class Motor
+{
+public:
+  int enPin, pin1, pin2;
+
+  Motor(int enPinNr, int pin1Nr, int pin2Nr)
+  {
+    enPin = enPinNr;
+    pin1 = pin1Nr;
+    pin2 = pin2Nr;
+  }
+
+  void setup()
+  {
+    pinMode(pin1, OUTPUT);
+    pinMode(pin2, OUTPUT);
+  }
+
+  void go(bool cc, int speed_control)
+  {
+    int high, low;
+
+    if (cc)
+    {
+      high = pin2;
+      low = pin1;
+    }
+
+    else
+    {
+      high = pin1;
+      low = pin2;
+    }
+
+    int speed = map(speed_control, 0, pin2, 0, 255);
+    analogWrite(enPin, speed);
+
+    digitalWrite(low, LOW);
+    digitalWrite(high, HIGH);
+  }
+};
+
+Motor leftMotor = Motor(11, 12, 13);
+Motor rightMotor = Motor(3, 2, 4);
 
 void setup()
 {
-  pinMode(pin1, OUTPUT);
-  pinMode(pin2, OUTPUT);
+  leftMotor.setup();
+  rightMotor.setup();
 }
 
 void loop()
 {
-  go(true, 2);
-  //delay(2000);
-  //go(false, pin2);
-  //delay(2000);
-}
-
-void go(bool cc, int speed_control)
-{
-  int high, low;
-
-  if (cc)
-  {
-    high = pin2;
-    low = pin1;
-  }
-  else
-  {
-    high = pin1;
-    low = pin2;
-  }
-
-  int speed = map(speed_control, 0, pin2, 0, 255);
-  analogWrite(enPin, speed);
-
-  digitalWrite(low, LOW);
-  digitalWrite(high, HIGH);
+  leftMotor.go(false, 4);
+  rightMotor.go(true, 4);
 }
