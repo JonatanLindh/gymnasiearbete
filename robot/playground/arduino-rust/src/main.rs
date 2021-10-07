9ce0d20e02e182d9c6f13b7871377406265683cb
Jonatan Lindh <jonatan.lindh1@gmail.com>
2021-10-07 13:18:39 +0200

    rust-analyzer target

--- robot/playground/arduino-rust/src/main.rs
+++ robot/playground/arduino-rust/src/main.rs
@@ -4,7 +4,7 @@
 use arduino_hal::prelude::*;
 use panic_halt as _;
 
-#[arduino_hal::entry]
+// #[arduino_hal::entry]
 fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
