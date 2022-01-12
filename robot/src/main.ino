b39d9ce34197b242b09b774e4a6e76d437ff8a99
Jonatan Lindh <jonatan.lindh1@gmail.com>
2022-01-12 22:01:56 +0100

    add motor control

--- robot/src/main.ino
+++ robot/src/main.ino
@@ -1,10 +1,21 @@
 #include "TCS34725_Color_Sensor.h"
 #include <Wire.h>
 
+// Multiplexer settings
 #define TCAADDR 0x70
 #define CSLAddr 0
 #define CSRAddr 1
 #define CSMAddr 2
+
+// Left Motor Pins
+#define MOTOR_L_EN_PIN 9
+#define MOTOR_L_PIN1 7
+#define MOTOR_L_PIN2 8
+
+// Right Motor Pins
+#define MOTOR_R_EN_PIN 3
+#define MOTOR_R_PIN1 2
+#define MOTOR_R_PIN2 4
 
 class ColorSensor
 {
@@ -66,6 +77,63 @@ private:
     }
 };
 
+class Motor
+{
+public:
+    int enPin, pin1, pin2;
+
+    Motor(int enPinNum, int pin1Num, int pin2Num)
+    {
+        enPin = enPinNum;
+        pin1 = pin1Num;
+        pin2 = pin2Num;
+    }
+
+    enum Direction : int
+    {
+        Forward,
+        Backward,
+    };
+
+    void setup()
+    {
+        pinMode(pin1, OUTPUT);
+        pinMode(pin2, OUTPUT);
+    }
+
+    void start(Direction direction, int motor_speed)
+    {
+        int high, low;
+
+        if (direction == Forward)
+        // if (dir)
+        {
+            high = pin1;
+            low = pin2;
+        }
+
+        else
+        {
+            high = pin2;
+            low = pin1;
+        }
+
+        analogWrite(enPin, motor_speed);
+
+        digitalWrite(low, LOW);
+        digitalWrite(high, HIGH);
+    }
+    void stop()
+    {
+        digitalWrite(pin1, LOW);
+        digitalWrite(pin2, LOW);
+        analogWrite(enPin, 0);
+    }
+};
+
+Motor leftMotor = Motor(MOTOR_L_EN_PIN, MOTOR_L_PIN1, MOTOR_L_PIN2);
+Motor rightMotor = Motor(MOTOR_R_EN_PIN, MOTOR_R_PIN1, MOTOR_R_PIN2);
+
 ColorSensor colorSensorL = ColorSensor("Left", CSLAddr);
 ColorSensor colorSensorR = ColorSensor("Right", CSRAddr);
 ColorSensor colorSensorM = ColorSensor("Middle", CSMAddr);
@@ -73,27 +141,32 @@ ColorSensor colorSensorM = ColorSensor("Middle", CSMAddr);
 void setup()
 {
     Wire.begin();
-
     Serial.begin(9600);
-    while (!Serial)
-        ;
 
     colorSensorL.setup();
     colorSensorR.setup();
     colorSensorM.setup();
+
+    leftMotor.setup();
+    rightMotor.setup();
 }
 
 void loop()
 {
-
+    // Read all color sensors
     colorSensorL.read();
     colorSensorR.read();
     colorSensorM.read();
 
+    // Print all color sensor values
     colorSensorL.print();
     colorSensorR.print();
     colorSensorM.print();
     Serial.println();
+
+    // Move the robot
+    rightMotor.start(Motor::Forward, 9);
+    leftMotor.start(Motor::Forward, 9);
 
     delay(3000);
 }
